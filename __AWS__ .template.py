918075058f1c9dc35a52b24b2b94df1d47af1240
Patrick Janssen <akiphtj@nus.edu.sg>
2019-11-21 10:14:12 +0800

    added Github auth

--- __AWS__ .template.py
+++ __AWS__ .template.py
@@ -3,5 +3,6 @@ S3_bucket = ""
 
 # your access key id
 aws_access_key_id = ''
+
 # your secret access key
 aws_secret_access_key = ''
---
+++ __GITHUB__ .py
@@ -0,0 +1,8 @@
+# Githib repo name
+github_repo = ""
+
+# your access key id
+github_access_key_id = ''
+
+# your secret access key
+github_secret_access_key = ''
