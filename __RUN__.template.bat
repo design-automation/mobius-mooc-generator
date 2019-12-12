2d99637a951c65a4e8f1a1b0ca43819aae5f059d
Patrick Janssen <akiphtj@nus.edu.sg>
2019-12-12 14:39:43 +0800

    updated to use double quotes

--- __RUN__.template.bat
+++ __RUN__.template.bat
@@ -1,3 +1,3 @@
-python ./mob_uploader.py 'C:/xxxx/mooc1-procedural-modelling'
+python ./mob_uploader.py "C:/xxxx/mooc1-procedural-modelling"
 
-python ./edx_generator.py 'C:/xxxx/mooc1-procedural-modelling' 'C:/Data/xxxx/mooc1'
+python ./edx_generator.py "C:/xxxx/mooc1-procedural-modelling" "C:/Data/xxxx/mooc1"
