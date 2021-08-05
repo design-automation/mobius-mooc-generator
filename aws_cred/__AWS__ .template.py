e4146a90e27a48a120a5e310be8be52acb4867b2
phtj <patrick@janssen.name>
2021-08-05 18:19:41 +0800

    minor update

--- aws_cred/__AWS__ .template.py
+++ aws_cred/__AWS__ .template.py
@@ -1,4 +1,5 @@
-# Add your AWS credentials for uploading files to your S3 bucket
+# Rename this file to __AWS__.py
+# Then add your AWS credentials for uploading files to your S3 bucket
 
 # your access key id
 aws_access_key_id = ''
